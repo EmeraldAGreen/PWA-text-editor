dfe8dbf96f8adccebf522ec19710e0340855d7c9
Emerge <egreengirl1@gmail.com>
2022-05-09 22:04:42 -0600

    regenerator runtime errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,9 +25,22 @@ module.exports = () => {
         swDest: 'service-worker.js',
       }),
       new WebpackPwaManifest({
-        // TODO: Create a manifest.json:
+        // fingerprints: false,
+        // inject: true,
+        // background_color: '#225ca3',
+        // theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
+  
 // TODO: Add CSS loaders and babel to webpack. :)
     module: {
       rules: [
